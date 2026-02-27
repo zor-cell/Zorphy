ba9e56e1d7b231827b9c9e2acf9973760ce31553
zor-cell <philippemanu.zorman@gmail.com>
2026-02-27 21:38:35 +0100

    removed mobile directories

--- backend/src/main/java/net/zorphy/backend/config/http/WebConfig.java
+++ backend/src/main/java/net/zorphy/backend/config/http/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://zorphy.net")
+                .allowedOrigins("http://localhost", "http://localhost:4200", "https://zorphy.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
--- frontend/android/app/src/main/java/net/zorphy/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package net.zorphy;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
